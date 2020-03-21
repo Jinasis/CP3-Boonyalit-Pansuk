60c58b10e31f75a960522c663d34c196490d5b11
Jinasis <da4gon@hotmail.com>
2020-03-21 16:01:25 +0700

    Create Exercise4

---
+++ Exercise4_Boonyalit_P.py
@@ -0,0 +1,10 @@
+FE = 10.0
+GB = 7.5
+ICS = 8.5
+CP = 5.5
+
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
